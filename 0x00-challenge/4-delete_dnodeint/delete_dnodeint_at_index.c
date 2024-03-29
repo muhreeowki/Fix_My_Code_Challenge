e22a415a947533bcdd5a7511ed3be140ce1e66a0
Muriuki <97036296+muhreeowki@users.noreply.github.com>
2024-03-29 11:14:08 +0300

    Making Corrections

--- 0x00-challenge/4-delete_dnodeint/delete_dnodeint_at_index.c
+++ 0x00-challenge/4-delete_dnodeint/delete_dnodeint_at_index.c
@@ -9,41 +9,45 @@
  *
  * Return: 1 on success, -1 on failure
  */
-int delete_dnodeint_at_index(dlistint_t **head, unsigned int index) {
-  dlistint_t *saved_head;
-  dlistint_t *tmp;
-  unsigned int p;
+int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
+{
+	dlistint_t *saved_head;
+	dlistint_t *tmp;
+	unsigned int p;
 
-  if (*head == NULL) {
-    return (-1);
-  }
-  if (0 == index) {
-    tmp = (*head)->next;
-    free(*head);
-    *head = tmp;
-    if (tmp != NULL) {
-      tmp->prev = NULL;
-    }
-    return (1);
-  }
-  saved_head = *head;
-  p = 0;
-  while (p < index && *head != NULL) {
-    *head = (*head)->next;
-    p++;
-  }
-  if (p != index) {
-    *head = saved_head;
-    return (-1);
-  } else {
-    if ((*head)->next) {
-      (*head)->prev->next = (*head)->next;
-      (*head)->next->prev = (*head)->prev;
-    } else {
-      (*head)->prev->next = NULL;
-    }
-    free(*head);
-    *head = saved_head;
-  }
-  return (1);
+	if (*head == NULL)
+	{
+		return (-1);
+	}
+	saved_head = *head;
+	p = 0;
+	while (p < index && *head != NULL)
+	{
+		*head = (*head)->next;
+		p++;
+	}
+	if (p != index)
+	{
+		*head = saved_head;
+		return (-1);
+	}
+	if (0 == index)
+	{
+		tmp = (*head)->next;
+		free(*head);
+		*head = tmp;
+		if (tmp != NULL)
+		{
+			tmp->prev = NULL;
+		}
+	}
+	else
+	{
+		(*head)->prev->next = (*head)->next;
+		free(*head);
+		if ((*head)->next)
+			(*head)->next->prev = (*head)->prev;
+		*head = saved_head;
+	}
+	return (1);
 }
